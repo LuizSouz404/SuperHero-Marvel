2b314f644ee62ddfa8b2dcdb63982b91ccd479f3
LuizSouz404 <luiz.souz404@gmail.com>
2022-03-10 17:00:00 -0300

    chore: add react-icons

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import { BsPersonFill, BsBookFill,BsPencilFill, BsHouse } from 'react-icons/bs';
+import { GiLightSabers, GiBookshelf, GiPickOfDestiny } from 'react-icons/gi';
+
+export function Header() {
+  return (
+    <header className="w-60 min-h-screen bg-gray-800 shadow-md p-4">
+      <div className="flex flex-col gap-10 h-full w-full px-4">
+        <img className="w-full" src="/logo.png" alt="Marvel" />
+
+        <ul className='flex flex-col gap-1 text-white px-2'>
+          <Link href="#" passHref>
+            <li className='bg-red-600 p-2 rounded-md shadow-md shadow-red-600/50 cursor-pointer flex items-center gap-4 text-lg font-normal'><BsHouse size={22}/>Home</li>
+          </Link>
+          <Link href="#" passHref>
+            <li className='p-2 cursor-pointer flex items-center gap-4 text-lg font-normal'><BsPersonFill size={22}/>Characters</li>
+          </Link>
+          <Link href="#" passHref>
+            <li className='p-2 rounded-md cursor-pointer flex items-center gap-4 text-lg font-normal'><BsBookFill size={22}/>Comics</li>
+          </Link>
+          <Link href="#" passHref>
+            <li className='p-2 rounded-md cursor-pointer flex items-center gap-4 text-lg font-normal'><BsPencilFill size={22}/>Creators</li>
+          </Link>
+          <Link href="#" passHref>
+            <li className='p-2 rounded-md cursor-pointer flex items-center gap-4 text-lg font-normal'><GiLightSabers size={22}/>Events</li>
+          </Link>
+          <Link href="#" passHref>
+            <li className='p-2 rounded-md cursor-pointer flex items-center gap-4 text-lg font-normal'><GiBookshelf size={22}/>Series</li>
+          </Link>
+          <Link href="#" passHref>
+            <li className='p-2 rounded-md cursor-pointer flex items-center gap-4 text-lg font-normal'><GiPickOfDestiny size={22}/>Stories</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,22 @@
 import type { NextPage } from 'next'
 import { useEffect } from 'react'
 import { api } from '../service/api'
-import styles from '../styles/Home.module.css'
 import crypto from 'crypto-js';
+import { Header } from '../components/Header';
 
 const Home: NextPage = () => {
-  useEffect(() => {
+  useEffect(() => {/*
     const timestamp = Date.now();
     const formatHash = `${timestamp}2a4b85951d73a572e94a755d4262a654df6ea9b605805841a2d5bf33286642e479718a54`
     const Hash = crypto.MD5(formatHash)
     const data = api.get(`characters?ts=${timestamp}&apikey=05805841a2d5bf33286642e479718a54&hash=${Hash}
     `).then(response => console.log(response.data.data.results))
+    */
   }, []);
 
   return (
-    <div className={styles.container}>
+    <div className="min-h-screen w-full">
+      <Header />
       
     </div>
   )
