2e0ca28f10d0c98c2dee7a6237c7748e7644cacd
LuizSouz404 <luiz.souz404@gmail.com>
2022-03-12 00:33:58 -0300

    feat: updated header navigation to mark which page we are located

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,41 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { BsPersonFill, BsBookFill,BsPencilFill, BsHouse } from 'react-icons/bs';
 import { GiLightSabers, GiBookshelf, GiPickOfDestiny } from 'react-icons/gi';
+import { SearchInput } from './SearchInput';
 
 export function Header() {
+  const router = useRouter();
+  const currentPath = router.pathname.replace("/", "");
+
   return (
-    <header className=" w-80 min-h-screen bg-gray-800 shadow-md p-4">
-      <div className="fixed flex flex-col gap-10 h-full w-56 px-4">
+    <header className="fixed min-h-screen bg-gray-800 shadow-md p-4">
+      <div className=" flex flex-col gap-10 h-full w-56 px-4">
         <img className="w-full" src="/logo.png" alt="Marvel" />
 
+        <SearchInput />
+
         <ul className='flex flex-col gap-1 text-white px-2'>
-          <Link href="#" passHref>
-            <li className='bg-red-600 p-2 rounded-md shadow-md shadow-red-600/50 cursor-pointer flex items-center gap-4 text-lg font-normal'><BsHouse size={22}/>Home</li>
+          <Link href="/" passHref>
+            <li className={`${!currentPath ? 'bg-red-600 shadow-red-600/50 shadow-md': ''} p-2 rounded-md cursor-pointer flex items-center gap-4 text-lg font-normal`}><BsHouse size={22}/>Home</li>
           </Link>
-          <Link href="#" passHref>
-            <li className='p-2 cursor-pointer flex items-center gap-4 text-lg font-normal'><BsPersonFill size={22}/>Characters</li>
+          <Link href="/characters" passHref>
+            <li className={`${currentPath === 'characters' ? 'bg-red-600 shadow-red-600/50 shadow-md': ''} p-2 rounded-md cursor-pointer flex items-center gap-4 text-lg font-normal`}><BsPersonFill size={22}/>Characters</li>
           </Link> 
-          <Link href="#" passHref>
-            <li className='p-2 rounded-md cursor-pointer flex items-center gap-4 text-lg font-normal'><BsBookFill size={22}/>Comics</li>
+          <Link href="/comics" passHref>
+            <li className={`${currentPath === 'comics' ? 'bg-red-600 shadow-red-600/50 shadow-md': ''} p-2 rounded-md cursor-pointer flex items-center gap-4 text-lg font-normal`}><BsBookFill size={22}/>Comics</li>
           </Link>
-          <Link href="#" passHref>
-            <li className='p-2 rounded-md cursor-pointer flex items-center gap-4 text-lg font-normal'><BsPencilFill size={22}/>Creators</li>
+          <Link href="/creators" passHref>
+            <li className={`${currentPath === "creators" ? 'bg-red-600 shadow-red-600/50 shadow-md': ''} p-2 rounded-md cursor-pointer flex items-center gap-4 text-lg font-normal`}><BsPencilFill size={22}/>Creators</li>
           </Link>
-          <Link href="#" passHref>
-            <li className='p-2 rounded-md cursor-pointer flex items-center gap-4 text-lg font-normal'><GiLightSabers size={22}/>Events</li>
+          <Link href="/events" passHref>
+            <li className={`${currentPath === "events" ? 'bg-red-600 shadow-red-600/50 shadow-md': ''} p-2 rounded-md cursor-pointer flex items-center gap-4 text-lg font-normal`}><GiLightSabers size={22}/>Events</li>
           </Link>
-          <Link href="#" passHref>
-            <li className='p-2 rounded-md cursor-pointer flex items-center gap-4 text-lg font-normal'><GiBookshelf size={22}/>Series</li>
+          <Link href="/series" passHref>
+            <li className={`${currentPath === "series" ? 'bg-red-600 shadow-red-600/50 shadow-md': ''} p-2 rounded-md cursor-pointer flex items-center gap-4 text-lg font-normal`}><GiBookshelf size={22}/>Series</li>
           </Link>
-          <Link href="#" passHref>
-            <li className='p-2 rounded-md cursor-pointer flex items-center gap-4 text-lg font-normal'><GiPickOfDestiny size={22}/>Stories</li>
+          <Link href="/stories" passHref>
+            <li className={`${currentPath === "stories" ? 'bg-red-600 shadow-red-600/50 shadow-md': ''} p-2 rounded-md cursor-pointer flex items-center gap-4 text-lg font-normal`}><GiPickOfDestiny size={22}/>Stories</li>
           </Link>
         </ul>
       </div>
