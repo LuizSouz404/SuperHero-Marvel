386488bca8d5e3380ccf69a8388e16ec79d6506c
LuizSouz404 <luiz.souz404@gmail.com>
2022-03-13 02:09:03 -0300

    feat: Loader component, svg filled loader(Mt pica tá?)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next'
 import { Header } from '../components/Header';
+import { Loader } from '../components/Loader';
 import { PageHome } from '../components/PageHome';
 
 const Home: NextPage = () => {
 
   return (
     <div className="min-h-screen h-full w-full flex">
+      <Loader />
       <Header />
       
       <PageHome />
