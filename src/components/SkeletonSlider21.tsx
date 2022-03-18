7b6c0a94f3ccf02725ca5be1c90691b3b4909ca0
LuizSouz404 <luiz.souz404@gmail.com>
2022-03-18 18:03:59 -0300

    feat: created skeletonslide Component

---
+++ src/components/SkeletonSlider21.tsx
@@ -0,0 +1,24 @@
+import { motion } from 'framer-motion';
+
+type SkeletonSlider21Props = {
+  title: string;
+  isSquare?: boolean;
+}
+
+export function SkeletonSlider21({title ,isSquare = false}: SkeletonSlider21Props) {
+  return (
+    <div className="flex flex-col w-full gap-4">
+      <strong className="text-white font-semibold text-xl">title</strong>
+      <motion.div className="overflow-hidden">
+        <motion.div drag="x" dragConstraints={{right: 0, left: -1024}} className="grid grid-cols-auto gap-2 grid-flow-col">
+          <div className={`bg-slate-600 animate-pulse w-52 ${isSquare ? 'aspect-square' : 'aspect-2/1'} rounded-md`}></div>
+          <div className={`bg-slate-600 animate-pulse w-52 ${isSquare ? 'aspect-square' : 'aspect-2/1'} rounded-md`}></div>
+          <div className={`bg-slate-600 animate-pulse w-52 ${isSquare ? 'aspect-square' : 'aspect-2/1'} rounded-md`}></div>
+          <div className={`bg-slate-600 animate-pulse w-52 ${isSquare ? 'aspect-square' : 'aspect-2/1'} rounded-md`}></div>
+          <div className={`bg-slate-600 animate-pulse w-52 ${isSquare ? 'aspect-square' : 'aspect-2/1'} rounded-md`}></div>
+          <div className={`bg-slate-600 animate-pulse w-52 ${isSquare ? 'aspect-square' : 'aspect-2/1'} rounded-md`}></div>
+        </motion.div>
+      </motion.div>
+    </div>
+  )
+}
