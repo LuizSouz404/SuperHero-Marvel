e60582102b1e66a6f53f86d06f97735b951192e4
LuizSouz404 <luiz.souz404@gmail.com>
2022-03-18 21:04:58 -0300

    fix: fixing search input bug

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -65,7 +65,7 @@ export function SearchInput() {
       {input !== "" && heros && (
         <ul className='flex flex-col w-full bg-slate-800 rounded-b-md border-red-600 border-t-0 border-2 absolute top-full left-0'>
           {heros?.map((hero, index) => (
-            <Link href={`characters/${hero.id}`} passHref key={index}>
+            <Link href={`/${currentPath  === "" ? 'characters' : currentPath }/${hero.id}`} passHref key={index}>
               <li key={index} className={`flex items-center hover:bg-slate-900 cursor-pointer gap-2 px-4 text-ellipsis p-2 overflow-hidden ${heros.length !== index + 1 ? 'border-b-2 border-slate-400/10' : ''}`}>
                 <img className='w-10 h-10 object-cover aspect-square rounded-full' src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`} alt={searchDynamicPath === 'titleStartsWith' ? hero.title : currentPath === "creators" ? hero.firstName : hero.name} />
                 <span className='text-sm text-white text-ellipsis  whitespace-nowrap overflow-hidden w-auto'>{searchDynamicPath === 'titleStartsWith' || currentPath === 'events' ? hero.title : currentPath === "creators" ? hero.firstName : hero.name}</span>
